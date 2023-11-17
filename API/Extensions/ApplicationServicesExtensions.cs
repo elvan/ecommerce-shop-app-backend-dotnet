dcb376464e34521483c387795e973bcf418bcf4d
Elvan Hidayat <elvanhd@gmail.com>
2023-11-17 23:36:18 +0700

    adding the basket feature

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -7,6 +7,8 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+using StackExchange.Redis;
+
 namespace API.Extensions;
 
 public static class ApplicationServicesExtensions
@@ -21,6 +23,12 @@ public static class ApplicationServicesExtensions
         {
             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
+        services.AddSingleton<IConnectionMultiplexer>(c =>
+        {
+            var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+            return ConnectionMultiplexer.Connect(options);
+        });
+        services.AddScoped<IBasketRepository, BasketRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
