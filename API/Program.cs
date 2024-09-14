a8757e1be9daae6d191555b890496534364a0fba
Elvan Hidayat <elvanhd@gmail.com>
2024-09-14 07:55:17 +0700

    Add global error handling and exception middleware
    - Implement ExceptionMiddleware to handle exceptions globally
    - Create ApiErrorResponse for consistent error responses
    - Add BuggyController for testing error responses
    - Create validation for CreateProductDto with data annotations
    - Add CORS configuration to allow requests from specified origins
    - Update Program.cs to use ExceptionMiddleware and CORS policy

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using API.Middleware;
+
 using Core.Interfaces;
 
 using Infrastructure.Data;
@@ -15,10 +17,15 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
